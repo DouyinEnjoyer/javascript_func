458f089bfe834c8f7ccbf98ec3246da32475ec6d
Dobrossy Levente <dobrossy.levente@BOLYAI.LOCAL>
2025-10-17 09:39:04 +0200

    javvvvvva

---
+++ table_col_hf/kitero.js
@@ -0,0 +1,48 @@
+/**
+ * ez a fuggveny valamit csinal
+ * @returns {void}
+  */
+
+const szam = 6
+
+function valami1()
+{
+    console.log("gooba")
+}
+valami1()
+
+function valami2()
+{
+    console.log(szam)
+}
+valami2()
+/**
+ * ez egy kolalis valtozo
+ */
+function valami3()
+{
+
+
+    const v = 9
+
+    console.log(v)
+}
+valami3()
+
+/**
+ * valamit csinal
+ * 
+ * @param {number} sigma 
+ * 
+ * @returns {number}
+ */
+
+function valami4(sigma)
+{
+    const a = 6
+    const c = a + sigma
+    return c
+}
+const opsum = valami4(2)
+console.log(opsum)
+
